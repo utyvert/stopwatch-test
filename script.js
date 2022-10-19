c2c07837abf67a8ab97e9971d34d8501961639c8
utyvert <utkarshuppal@gmail.com>
2022-10-19 00:41:05 -0700

    working on a random stopwatch project

---
+++ script.js
@@ -0,0 +1,25 @@
+const startButton = $('#start-button');
+const stopButton = $('#stop-button');
+const resetButton = $('#reset-button');
+
+const secondsTime = $('#seconds');
+const minutesTime = $('#minutes')
+const hoursTime = $('#hours')
+
+
+
+startButton.on('click', startWatch);
+stopButton.on('click', stopWatch);
+resetButton.on('click', resetWatch);
+
+function startWatch() { // start timer
+
+}
+
+function stopWatch() { // stop timer
+
+}
+
+function resetWatch() { // reset timer
+
+}
